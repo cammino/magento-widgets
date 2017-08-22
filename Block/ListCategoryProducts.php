75c190a379bb73ec1284fe6aff068cc92565f9c4
Luan Dantas <kidinoop@hotmail.com>
2017-08-22 17:21:22 -0300

    Remove arquivos para renomear e resolver problema com camelcase

--- Block/ListCategoryProducts.php
+++
@@ -1,3 +0,0 @@
-<?php
-class Cammino_Widgets_Block_ListCategoryProducts extends Mage_Catalog_Block_Product_List {
-}
--- Block/MiniBanners.php
+++
@@ -1,3 +0,0 @@
-<?php
-class Cammino_Widgets_Block_MiniBanners extends Mage_Page_Block_Html {
-}
