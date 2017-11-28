8fc1af27e1922f81bff87d62d7109a5e837d70f2
Marcio Junior <marciofmjr@hotmail.com>
2017-11-28 10:30:52 -0200

    Ajusta para receber parametros do formato da tag de desconto por config no admin

--- Helper/Data.php
+++ Helper/Data.php
@@ -15,14 +15,18 @@ class Cammino_Widgets_Helper_Data extends Mage_Core_Helper_Abstract
         return $percentage;
 	}
 
-	public function getDiscountBoxHtml($product, $label = "% OFF"){
+	public function getDiscountBoxHtml($product){
+		$labelBefore = Mage::getStoreConfig('themeconfig/themeconfig_group_product_item/themeconfig_product_item_discount_label_before');
+		$labelAfter = Mage::getStoreConfig('themeconfig/themeconfig_group_product_item/themeconfig_product_item_discount_label_after');
+
 		$discount = $this->getPercentageDiscount($product);
 		$html = "";
 
 		if($discount > 0){
 			$html = "<div class='discount-box'>";
-			$html .= 	"<div class='value'>" . $discount . "</div>";
-			$html .= 	"<div class='label'>" . $label . "</div>";
+				if(strlen($labelBefore) > 0){ $html .= 	"<div class='label before'>" . $labelBefore . "</div>";	}
+				$html .= 	"<div class='value'>" . $discount . "</div>";
+				if(strlen($labelAfter) > 0){ $html .= 	"<div class='label after'>" . $labelAfter . "</div>"; }
 			$html .= "</div>";
 		}else{
 			$html = "";
