ec984b7ea8c41e50f298c56dc4ec0ccac1f1db58
Marcelo Ferracioli <marceloferracioli@gmail.com>
2019-01-29 18:07:34 -0200

    Define ordenção da lista de produtos.

--- Block/Listcategoryproducts.php
+++ Block/Listcategoryproducts.php
@@ -1,3 +1,12 @@
 <?php
 class Cammino_Widgets_Block_Listcategoryproducts extends Mage_Catalog_Block_Product_List {
+
+	public function getSortBy() {
+		return "position";
+	}
+
+	public function getDefaultDirection() {
+		return "asc";
+	}
+
 }
