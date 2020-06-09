ab1fcd442ce61193fd7cc550d2d620a766d1dbce
Luan Dantas <luandr92@gmail.com>
2020-06-09 14:53:49 -0300

    #11041 - Implement rule to order home product lists by category default sort by from admin

--- Block/Listcategoryproducts.php
+++ Block/Listcategoryproducts.php
@@ -2,7 +2,9 @@
 class Cammino_Widgets_Block_Listcategoryproducts extends Mage_Catalog_Block_Product_List {
 
 	public function getSortBy() {
-		return "position";
+		$category = Mage::getModel('catalog/category')->load($this->getCategoryId());
+
+		return $category->getDefaultSortBy();
 	}
 
 	public function getDefaultDirection() {
